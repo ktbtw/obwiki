4c9d0184f5ed403ad1c41d47d29ca7675cd7050d
自由人 <15954831+free-jiejiejie@user.noreply.gitee.com>
2025-07-02 17:26:49 +0800

    优化了网络请求

--- obwiki-frontend/src/api/index.ts
+++ obwiki-frontend/src/api/index.ts
@@ -1,5 +1,13 @@
 // 初始化axios
+import store from '@/store';
+import { Modal } from 'ant-design-vue';
 import axios from 'axios';
+
+declare global {
+  interface Window {
+    __hasLoginModal?: boolean;
+  }
+}
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8880/obwiki", // 使用环境变量设置基础URL
@@ -10,19 +18,33 @@ const api = axios.create({
 /*
 * 使用axios拦截器打印请求和响应的数据
 * */
-api.interceptors.request.use(function (config){
-  // console.log('请求参数：',config);
 
+
+api.interceptors.request.use(function (config: any) {
+  // 判断是否需要登录
+  if(config.url.includes('/login')||config.url.includes('/save')){
+    return config;
+  }
+    const token = store.state.user.token;
+    if (!token) {
+      if (!window.__hasLoginModal) {
+        window.__hasLoginModal = true;
+        Modal.warning({
+          title: '请先登录',
+          content: '您需要登录后才能进行此操作',
+          onOk: () => {
+            window.__hasLoginModal = false;
+          }
+        });
+      }
+      // 阻止请求
+      return new Promise(() => {});
+    }
+  if (token) {
+    config.headers.token = token;
+  }
   return config;
-},error => {
-  return Promise.reject(error);
-});
-
-api.interceptors.response.use(function (response){
-  // console.log('返回结果：',response);
-  return response;
-},error => {
-  // console.log('返回错误：',error);
+}, error => {
   return Promise.reject(error);
 });
 
